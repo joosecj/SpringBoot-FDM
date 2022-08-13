87817c383355bcf4ac19b6854f157614ece48f8c
Jose Carlos <joosecj@Jose.localdomain>
2022-08-13 18:20:16 -0300

    implement CommandLineRunner

--- DasafioUm/src/main/java/com/devsuperior/dasafioum/DasafioUmApplication.java
+++ DasafioUm/src/main/java/com/devsuperior/dasafioum/DasafioUmApplication.java
@@ -1,13 +1,18 @@
 package com.devsuperior.dasafioum;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DasafioUmApplication {
+public class DasafioUmApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(DasafioUmApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Olá mundo!");
+    }
 }
