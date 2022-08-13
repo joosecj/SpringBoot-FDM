1640065d75b446caac3c09f6521bf7c08ad222e3
Jose Carlos <joosecj@Jose.localdomain>
2022-08-13 18:14:12 -0300

    starting challenge

---
+++ DasafioUm/src/main/java/com/devsuperior/dasafioum/DasafioUmApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dasafioum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasafioUmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DasafioUmApplication.class, args);
+    }
+
+}
---
+++ DasafioUm/src/test/java/com/devsuperior/dasafioum/DasafioUmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dasafioum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DasafioUmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
