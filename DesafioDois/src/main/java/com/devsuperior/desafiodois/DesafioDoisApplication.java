94593e92dde621093cb00410e54aa16d6dc88a46
Jose Carlos <josecarloscjj@gmail.com>
2022-08-17 17:07:14 -0300

    starting challenge e setting H2

---
+++ DesafioDois/src/main/java/com/devsuperior/desafiodois/DesafioDoisApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafiodois;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDoisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDoisApplication.class, args);
+	}
+
+}
---
+++ DesafioDois/src/test/java/com/devsuperior/desafiodois/DesafioDoisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafiodois;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDoisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
