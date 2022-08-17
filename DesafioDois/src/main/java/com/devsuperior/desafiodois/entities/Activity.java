c6665f7edf04f8e5e3ee05b686795bb49c95ea65
Jose Carlos <josecarloscjj@gmail.com>
2022-08-17 17:31:21 -0300

    implemented Activity, Category

---
+++ DesafioDois/src/main/java/com/devsuperior/desafiodois/entities/Activity.java
@@ -0,0 +1,60 @@
+package com.devsuperior.desafiodois.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_activity")
+public class Activity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    public Activity() {
+    }
+
+    public Activity(Long id, String name, String description, Double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
---
+++ DesafioDois/src/main/java/com/devsuperior/desafiodois/entities/Category.java
@@ -0,0 +1,42 @@
+package com.devsuperior.desafiodois.entities;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    @OneToMany(mappedBy = "category")
+    private List<Activity> activities = new ArrayList<>();
+
+    public Category() {
+    }
+
+    public Category(Long id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
