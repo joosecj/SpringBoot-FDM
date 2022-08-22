c7fee2e58c88d39b6a030245903afe5e51090d01
Jose Carlos <josecarloscjj@gmail.com>
2022-08-22 17:59:38 -0300

    Setting H2

---
+++ DesafioTres/src/main/java/com/devsuperior/desafiotres/DesafioTresApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafiotres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioTresApplication.class, args);
+    }
+
+}
