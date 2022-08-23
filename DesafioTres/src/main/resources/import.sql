cb3bc256e9f890260c09c02e91079750849d9f7a
Jose Carlos <josecarloscjj@gmail.com>
2022-08-23 18:39:08 -0300

    SEED

---
+++ DesafioTres/src/main/resources/import.sql
@@ -0,0 +1,21 @@
+-- noinspection SqlResolveForFile @ table/"tb_client"
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Aderbar dos Santos', '31489004568', 12000.0, TIMESTAMP WITHOUT TIME ZONE '1990-03-17', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Léo Pereira', '14523678900', 22000.0, TIMESTAMP WITHOUT TIME ZONE '1996-01-31', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('David Luiz', '32346006977', 111089.0, TIMESTAMP WITHOUT TIME ZONE '1987-07-22', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Rodinei Marcelo', '45678900123', 17450.0, TIMESTAMP WITHOUT TIME ZONE '1975-03-04', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Matheus França', '34567128900', 3540.0, TIMESTAMP WITHOUT TIME ZONE '2000-09-08', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Guillermo Varela Oliveira', '15678923400', 47450.0, TIMESTAMP WITHOUT TIME ZONE '1993-03-24', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Filipe Luís', '18900234567', 132089.0, TIMESTAMP WITHOUT TIME ZONE '1985-08-09', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Ayrton Lucas', '18902345670', 3540.0, TIMESTAMP WITHOUT TIME ZONE '1997-06-19', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Thiago Maia', '82134568900', 8540.0, TIMESTAMP WITHOUT TIME ZONE '1997-03-23', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('João Gomes', '56123478900', 2540.0, TIMESTAMP WITHOUT TIME ZONE '2001-02-12', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Arturo Vidal', '83184567800', 16400.0, TIMESTAMP WITHOUT TIME ZONE '1987-05-22', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Erick Pulgar', '19861234700', 15043.0, TIMESTAMP WITHOUT TIME ZONE '1994-01-15', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Everton Ribeiro', '17892345600', 3540.0, TIMESTAMP WITHOUT TIME ZONE '1994-02-05', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Giorgian De Arrascaeta', '56789012340', 3504.0, TIMESTAMP WITHOUT TIME ZONE '1982-01-01', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Gabriel Barbosa', '12789003456', 192089.0, TIMESTAMP WITHOUT TIME ZONE '1989-01-10', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Pedro Guilherme', '12345067890', 82089.0, TIMESTAMP WITHOUT TIME ZONE '1997-06-20', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Everton Soares', '25967823900', 72089.0, TIMESTAMP WITHOUT TIME ZONE '1996-03-22', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Mário Sérgio', '10967823605', 62089.0, TIMESTAMP WITHOUT TIME ZONE '1990-05-29', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Bruno Henrique', '11967823094', 142089.0, TIMESTAMP WITHOUT TIME ZONE '1990-05-29', 1);
